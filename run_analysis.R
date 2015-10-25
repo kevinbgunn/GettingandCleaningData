aebe79322cdb6a3c8c3f39ba24929f33332cb28c
kevinbgunn <kevinbgunn@gmail.com>
2015-10-25 17:42:04 -0400

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -5,6 +5,7 @@
 ##4.Appropriately labels the data set with descriptive variable names. 
 ##5.From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
 
+library(data.table)
 
 #activity labels
 activity_labels <- read.table("./UCI HAR Dataset/activity_labels.txt")[,2]
